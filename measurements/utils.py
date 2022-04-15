bcba43199688d81557ec7b92c1742aae5a8dee1c
harish <harishthakur4455@gmail.com>
2022-04-15 20:30:08 +0530

    install geopy
    measure distance

---
+++ measurements/utils.py
@@ -0,0 +1,11 @@
+from django.contrib.gis.geoip2 import GeoIP2
+
+# helper functions
+
+def get_geo(ip):
+    g = GeoIP2()
+    country = g.country(ip)
+    city = g.city(ip)
+    lat, lon = g.lat_lon(ip)
+    return country, city, lat, lon
+
--- measurements/views.py
+++ measurements/views.py
@@ -1,20 +1,48 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Measurement
 from .forms import MeasurementModelForm
+from geopy.geocoders import Nominatim
+from geopy.distance import geodesic
+from .utils import get_geo
 
 # Create your views here.
 
 def calculate_distance_view(request):
     obj = get_object_or_404(Measurement, id=1)
     form = MeasurementModelForm(request.POST or None)
+    geolocator = Nominatim(user_agent="measurements")
+    
+    ip = '72.14.207.99'
+    country, city, lat, lon = get_geo(ip)
+    # print('location', country)
+    # print('city', city)
+    # print('lat', lat)
+    # print('lon', lon)
+    
+    location = geolocator.geocode(city)
+    #print("###", location)
+    
+    l_lat = lat
+    l_lon = lon
+    pointA = (l_lat, l_lon)
     
     if form.is_valid():
         instance = form.save(commit=False)
-        instance.destination = form.cleaned_data.get('destination')
-        instance.location = 'Himachal Pradesh'
-        instance.distance = 1500.00
+        destination_ = form.cleaned_data.get('destination')
+        destination = geolocator.geocode(destination_)
+        #print(destination)
+        d_lat = destination.latitude
+        d_lon = destination.longitude
+        
+        pointB = (d_lat, d_lon)
+        
+        distance = round(geodesic(pointA, pointB).km, 2)
+        
+        instance.location = location
+        instance.distance = distance
         instance.save()
     
     context = {'distance': obj, 'form': form,}
     
     return render(request, 'measurements/main.html', context)
+
